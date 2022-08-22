fd1b6dfec7c4691f5af0919245d4438c280bb61b
Ahana1105 <77888005+Ahana1105@users.noreply.github.com>
2022-08-22 21:14:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,73 @@
-canvas = document.getElementById("my_Canvas");
-var ctx = canvas.getContext("2d");
-var color = "mediumturquoise";
+var draw_score= 0;
+var draw_timer= 0;
+var timer_check="";
+var answer_holder="";
+var drawn_sketch ="";
 
-ctx.beginPath();
-ctx.strokeStyle= color;
-ctx.lineWidth = 3;
-ctx.arc (100,100,50,0,2*Math.PI);
-ctx.stroke();
-
-canvas.addEventListener("mousedown" , my_mousedown);
-
-function my_mousedown(e) {
-    mouse_x = e.clientX-canvas.offsetLeft;
-    mouse_y = e.clientY-canvas.offsetTop;
-    color=document.getElementById("color_input").value;
-    circle(mouse_x,mouse_y);
+function setup() {
+    canvas = createCanvas(280, 240)
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
 }
 
-function circle(mouse_x, mouse_y) {
-    ctx.beginPath();
-    ctx.strokeStyle= color;
-    ctx.lineWidth = 3;
-    ctx.arc (mouse_x,mouse_y,50,0,2*Math.PI);
-    ctx.stroke();   
+
+function preload() {
+    classifier = ml5.imageClassifier('DoodleNet');
 }
 
-function clean() {
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+function clear_canvas() {
+    background("white");
+    random_number=Math.floor((Math.random() * quick_draw_data_set[random_number]))
+    sketch="quick_draw_data_set[random_number];"
+    document.getElementById('sketch_drawn').innerHTML = 'Sketch To be Drawn: ' + sketch;
 }
+
+
+function draw() {
+    strokeWeight(10);
+    stroke(0);
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    }
+    console.log(results);
+    drawn_sketch = results[0].label;
+    document.getElementById('your_sketch').innerHTML = 'Your Sketch: ' + drawn_sketch;
+  
+    document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+  }
+
+ 
+
+
+
+function check_sketch()
+{
+  draw_timer++;
+  document.getElementById('timer').innerHTML = 'Timer: ' + draw_timer;
+  console.log(draw_timer)
+  if(draw_timer > 400)
+    {
+      draw_timer = 0;
+      timer_check = "completed"
+    }
+    if(timer_check =="completed" || answer_holder == "set")
+    {
+      timer_check = "";
+      answer_holder = "";
+      updateCanvas();
+    }
+
+}
+
+
